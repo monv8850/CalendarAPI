24a84904a168ade614a38d82c4150c88e970ff66
Lin Lin Feng <monv8850@gmail.com>
2014-03-19 19:16:56 +0000

    Remove hard coded dates

--- CalendarEvents/CalendarEvents/CalendarEvents/Models/Calendar.cs
+++ CalendarEvents/CalendarEvents/CalendarEvents/Models/Calendar.cs
@@ -8,27 +8,7 @@ namespace CalendarEvents.Models
         public List<Entry> Entries { get; set; }
         public Calendar()
         {
-            Entries=new List<Entry>();
-            Entries.Add(new Entry
-            {
-                StartTime = DateTime.Parse("25/10/1990"),
-                EndTime = DateTime.Parse("25/10/1991"),
-                LocationUrl = "Nigeria"
-            });
-
-            Entries.Add(new Entry
-            {
-                StartTime = DateTime.Parse("1/1/2014"),
-                EndTime = DateTime.Parse("1/10/2025"),
-                LocationUrl = "Crimea"
-            });
-
-            Entries.Add(new Entry
-            {
-                StartTime = DateTime.Parse("1/1/2026").AddDays(18),
-                EndTime =DateTime.Now.AddDays(106),
-                LocationUrl = "Moscow"
-            });
+         
         }
     }
 }
