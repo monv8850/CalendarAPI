ce309de540cdfad0de3402b92e9e729ce12b537c
Lin Lin Feng <monv8850@gmail.com>
2014-03-19 20:20:57 +0000

    Fix route

--- CalendarEvents/CalendarEvents/CalendarEvents/App_Start/WebApiConfig.cs
+++ CalendarEvents/CalendarEvents/CalendarEvents/App_Start/WebApiConfig.cs
@@ -50,7 +50,7 @@ namespace CalendarEvents
 
             config.Routes.MapHttpRoute(
              name: "DeleteEntry",
-             routeTemplate: "api/{controller}/Entry/{action}",
+             routeTemplate: "api/{controller}/{action}",
              defaults: new { controller = "Calendar", action = "Delete" }
             );
         }
